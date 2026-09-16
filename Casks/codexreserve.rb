664cb7b86acb06f47c9d222b1ec7b2998b0f1232
Arnav Attri <yourarnav@users.noreply.github.com>
2026-09-16 18:07:06 +0530

    Cask: v2.0.1

--- Casks/codexreserve.rb
+++ Casks/codexreserve.rb
@@ -1,6 +1,6 @@
 cask "codexreserve" do
-  version "2.0.0"
-  sha256 "cb54df769ab09af5557bd5ec0e5b262546aacc0c753ad714d68380cd04f56a76"
+  version "2.0.1"
+  sha256 "6be2864ac5e916833b0f6dc6ab4f594991201823e47fb2a98df89e32f9308e5e"
 
   url "https://github.com/yourarnav/CodexReserve/releases/download/v#{version}/CodexReserve.dmg"
   name "CodexReserve"
