2b7d0fcc903001958799096a213178950d171611
Arnav Attri <yourarnav@users.noreply.github.com>
2026-09-17 14:21:41 +0530

    Cask: fix v2.0.7 sha (was 65 chars)

--- Casks/codexreserve.rb
+++ Casks/codexreserve.rb
@@ -1,6 +1,6 @@
 cask "codexreserve" do
   version "2.0.7"
-  sha256 "dbfa59908efe7e3391287a0fbcea9fc7da2bdeba9fc5b43823520f8f3c93cef33"
+  sha256 "dbfa59908efe7e3391287a0fbcea9fc7da2bdebcba5b43823520f8f3c93cef33"
 
   url "https://github.com/yourarnav/CodexReserve/releases/download/v#{version}/CodexReserve.dmg"
   name "CodexReserve"
